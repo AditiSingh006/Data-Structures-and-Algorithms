e6e8b28d6fb7449ab77598a813d319c7046f8672
AditiSingh006 <149304532+AditiSingh006@users.noreply.github.com>
2026-01-01 21:50:44 +0530

    Update Digits in factorial.cpp

--- Mathematics/Digits in factorial.cpp
+++ Mathematics/Digits in factorial.cpp
@@ -1,1 +1,9 @@
-
+int digitsInFactorial(int n) {
+        // code here
+        if(n==0||n==1) return 1;
+        double k = 0;
+        for(int i=2; i<=n; i++){
+             k = k + log10(i);
+        }
+        return floor(k)+1;
+    }
